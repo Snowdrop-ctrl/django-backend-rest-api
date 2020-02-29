368781449168663dd385880b5f22662cf1110fe9
nitesh <niteshd850@gmail.com>
2020-02-29 11:57:34 +0530

    created serialize and add get,post,put,patch,delete, request

--- profile_project/profile_app/views.py
+++ profile_project/profile_app/views.py
@@ -1,15 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
+from rest_framework import status
 
-
+from profile_app import serializers
 
 
 class helloview(APIView):
-
     # authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
-
+    # permission_classes = [permissions.IsAdminUser]
+    serializer_class = serializers.HelloSerializers
 
     def get(self,request,formate=None):
         view = [
@@ -19,3 +19,23 @@ class helloview(APIView):
         ]
 
         return Response({'massage':'dddddd','view':view})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            massage = f'hello {name}'
+            return Response({'massage':massage})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        return Response('opration {put}')
+
+    def patch(self, request, pk=None):
+        return Response('opration {patch}')
+
+    def delete(self, request, pk=None):
+        return Response('opration {delete}')
