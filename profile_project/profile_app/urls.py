a369bcdb75d4f992a4dc4d291ace4938f5cbfd87
nitesh <niteshd850@gmail.com>
2020-02-29 13:48:46 +0530

    viewset list,create,retrieve,update,partial_update,destroy

--- profile_project/profile_app/urls.py
+++ profile_project/profile_app/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+
 from profile_app import views
 
+router = DefaultRouter()
+router.register('view_set', views.Helloviewset, basename='view_set')
 
 urlpatterns = [
-    path('hello/',views.helloview.as_view())
+    path('hello/',views.helloview.as_view()),
+    path('',include(router.urls)),
 ]
--- profile_project/profile_app/views.py
+++ profile_project/profile_app/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from rest_framework import status
+from rest_framework import viewsets
 
 from profile_app import serializers
 
@@ -39,3 +40,40 @@ class helloview(APIView):
 
     def delete(self, request, pk=None):
         return Response('opration {delete}')
+
+
+class Helloviewset(viewsets.ViewSet):
+
+    serializer_class = serializers.HelloSerializers
+
+    def list(self, request):
+        a_view = [
+        'in the viewsets we do crud',
+        'it easy to use',
+        ]
+
+        return Response({'massage':'hello!','a_view':a_view})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            massage = f'hello {name}'
+
+            return Response({'massage':massage})
+
+    def retrieve(self, request, pk=None):
+        return Response({'massage':'retrieve'})
+
+
+    def update(self, request, pk=None):
+        return Response({'massage':'update'})
+
+
+    def partial_update(self, request, pk=None):
+        return Response({'massage':'partial update'})
+
+
+    def destroy(self, request, pk=None):
+        return Response({'massage':'distroy'})
